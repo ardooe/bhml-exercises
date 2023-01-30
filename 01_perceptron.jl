d2198e6a47eda60e97850ebf8394416edd6c8c1a
Arto Österman <ardooe@gmail.com>
2023-01-31 00:01:16 +0700

    Some cleanups

--- 01_perceptron.jl
+++ 01_perceptron.jl
@@ -1,8 +1,8 @@
 # Initialize weights with random numbers and return as a Vector.
-initweights(x, y = 1) = vec(rand(y, x))
+initweights(x, y=1) = vec(rand(y, x))
 
 # Heaviside step function
-step(z, θ = 0) = z < θ ? 0 : 1
+step(z, θ=0) = z < θ ? 0 : 1
 
 # Generic activation function
 activate(f, z) = f(z)
@@ -14,19 +14,18 @@ activate(f, z) = f(z)
 predict(x, w, f=step) = activate(f, ∑(w, x))
 
 # Train the Perceptron by adjusting weights based on desired output
-function train(x::Matrix{Float64}, labels::Vector{Float64}, α = 0.1)
+function train(x::Matrix{Float64}, labels::Vector{Float64}, α=0.1)
     rows = size(x)[1] # Number of rows in the dataset
     inputsize = size(x)[2] # Number of columns in one row (inputs)
     w = initweights(inputsize)
 
-    # Loop through all rows
     for (i, _) ∈ enumerate(1:1:rows)
         y = labels[i] # The desired output for this row (label)
-        data = x[i, :]
+        data = x[i, :] # Data in current row (vector)
 
         ŷ = predict(x[i, :], w) # Prediction with the current weights
        
-        w += α * (ŷ - y) * data
+        w += α * (ŷ - y) * data # Update the weights if necessary (ŷ - y > 0)
     end
     w # Return the weights
 end
