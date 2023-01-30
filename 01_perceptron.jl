40fea1f1d4427dddfebe5c1c2b4d240d148f40a7
Arto Österman <ardooe@gmail.com>
2023-01-30 13:07:04 +0700

    Make activation function more generic, for easier switch

--- 01_perceptron.jl
+++ 01_perceptron.jl
@@ -1,11 +1,15 @@
 # Initialize weights with random numbers, given matrix size
 initweights(x, y) = rand(y, x)
 
-# Heaviside step (activation) function
-activate(z) = z < 0 ? 0 : 1
+# Heaviside step function
+step(z) = z < 0 ? 0 : 1
+
+# Generic activation function
+activate(f, z) = f(z)
 
 # The function that sums the dot product of given inputs and weights
 ∑(x, w) = sum(.*(x, w))
 
 # Predict the output with given input and weights
-predict(x, w) = activate(∑(w, x))
+predict(x, w, f=step) = activate(f, ∑(w, x))
+
