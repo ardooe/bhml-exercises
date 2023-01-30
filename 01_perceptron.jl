5790c5936a2004c29cd40904bc624ce3f944cda3
Arto Österman <ardooe@gmail.com>
2023-01-30 12:41:35 +0700

    Add initweights function

--- 01_perceptron.jl
+++ 01_perceptron.jl
@@ -1,5 +1,8 @@
+# Initialize weights with random numbers, given matrix size
+initweights(x, y) = rand(y, x)
+
 # Heaviside step (activation) function
 activate(z) = z < 0 ? 0 : 1
 
-# The function that sums the dot product of inputs and weights
+# The function that sums the dot product of given inputs and weights
 ∑(x, w) = sum(.*(x, w))
